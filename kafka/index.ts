8a2594c53811ffc8ce94f67748eb2daf6c28e3c4
kushal-mindgrep <kushal@mindgrep.com>
2023-09-14 20:45:39 +0530

    fix: deleted unnecessary files

--- kafka/index.ts
+++
@@ -1,8 +0,0 @@
-import {PlainObject, GSDataSource} from "@godspeedsystems/core"
-import { Consumer, Kafka, Producer, logLevel as kafkaLogLevel } from 'kafkajs';
-
-class KafkaDataSource extends GSDataSource{
-  async initClient(): Promise<PlainObject>{
-
-  }
-}
