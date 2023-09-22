dd5ae6a13547d42c561f296b7deb1a07bbdafd83
gurjotkaur20 <gurjotkaur20@gmail.com>
2023-09-22 14:08:54 +0530

    make this plugin a CLI and added more functions

--- devops-plugins/k8s/src/index.ts
+++ devops-plugins/k8s/src/index.ts
@@ -2,29 +2,12 @@
 import path from "path";
 import { spawnSync } from "child_process";
 import fs from "fs";
-import fsExtras from "fs-extra";
 import os from "os";
+import yaml from 'yaml';
+import { Command } from "commander";
 
 const helmChartsName = "godspeedsystems"
 const helmChartsUrl = "https://godspeedsystems.github.io/helm-charts"
-
-export async function repo(
-  command: string
-) {
-  try {
-    if (command == 'add') {
-      spawnSync('helm', ['repo', command, helmChartsName, helmChartsUrl], { stdio: 'inherit' })
-    } else if (command == 'update') {
-      spawnSync('helm', ['repo', command, helmChartsName], { stdio: 'inherit' })
-    } else if (command == 'remove') {
-      spawnSync('helm', ['repo', command, helmChartsName], { stdio: 'inherit' })
-    } else {
-      throw new Error('Unsupported argument');
-    }
-  } catch (error: any) {
-    throw error;
-  }
-}
 
 export async function create_config(
   componentName: string,
@@ -35,7 +18,7 @@ export async function create_config(
     const srcPath = path.resolve(os.homedir(), `.godspeed/devops-plugins/node_modules/k8s/${componentName}/${fileName}`);
     
     // check if devops-plugin k8s is added or not.
-    if (!fsExtras.existsSync(srcPath)) {
+    if (!fs.existsSync(srcPath)) {
       throw new Error(`${srcPath} does not exist`);
     }
 
@@ -46,7 +29,7 @@ export async function create_config(
         path.resolve(process.cwd(), fileName)
       );
     } else {
-      const isDirExist = fsExtras.existsSync(configDirPath);
+      const isDirExist = fs.existsSync(configDirPath);
       const destPath = path.resolve(configDirPath, fileName);
 
       // check if provided path exists or not. If it exists then copy the config file else create the directory then copy the config file.
@@ -56,7 +39,7 @@ export async function create_config(
           destPath
         );  
       } else {
-        fsExtras.mkdirSync(path.resolve(configDirPath));
+        fs.mkdirSync(path.resolve(configDirPath), { recursive: true });
         fs.copyFileSync(
           srcPath,
           destPath
@@ -69,6 +52,103 @@ export async function create_config(
   }
 }
 
+export async function install(
+  componentName: string,
+  configFilePath: string
+) {
+  try {
+    // check if configFilePath exists or not
+    if (!fs.existsSync(configFilePath)) {
+      throw new Error(`${configFilePath} does not exist`);
+    }
+
+    const configFileYaml = yaml.parse(fs.readFileSync(configFilePath, { encoding: 'utf-8' }));
+
+    console.log(`helm install ${componentName} godspeedsystems/${componentName} -f ${configFilePath} --create-namespace -n ${configFileYaml.namespace}`);
+    spawnSync('helm', ['repo', 'add', helmChartsName, helmChartsUrl], { stdio: 'inherit' })
+    //spawnSync('helm', ['install', componentName, `godspeedsystems/${componentName}`, '-f', configFilePath], { stdio: 'inherit' })
+    spawnSync('helm', ['install', componentName, `godspeedsystems/${componentName}`, '-f', configFilePath, '--create-namespace', '-n', configFileYaml.namespace], { stdio: 'inherit' })
+  } catch (error: any) {
+    throw error;
+  }
+}
+
+export async function update(
+  componentName: string,
+  configFilePath: string
+) {
+  try {
+    //console.log(`helm upgrade ${componentName} godspeedsystems/${componentName} -f ${configFilePath}`);
+    spawnSync('helm', ['upgrade', componentName, `godspeedsystems/${componentName}`, '-f', configFilePath], { stdio: 'inherit' })
+  } catch (error: any) {
+    throw error;
+  }
+}
+
+export async function remove(
+  componentName: string,
+  configFilePath: string
+) {
+  try {
+    // check if configFilePath exists or not
+    if (!fs.existsSync(configFilePath)) {
+      throw new Error(`${configFilePath} does not exist`);
+    }
+
+    const configFileYaml = yaml.parse(fs.readFileSync(configFilePath, { encoding: 'utf-8' }));
+    //console.log(`helm upgrade ${componentName} godspeedsystems/${componentName} -f ${configFilePath}`);
+    spawnSync('helm', ['delete', componentName, '-n', configFileYaml.namespace], { stdio: 'inherit' })
+  } catch (error: any) {
+    throw error;
+  }
+}
+
 (async function main() {
-create_config("argo-cd", "./abc");
+
+  const program = new Command();
+  program.showHelpAfterError();
+  program.showSuggestionAfterError(true);
+
+  program
+  .command("create-config")
+  .description("create configuration file for the specified component")
+  .argument("<componentName>", "component name")
+  .action((componentName) => {
+    create_config(componentName);
+  });
+
+  program
+  .command("install")
+  .description("install the specified component on kubernetes")
+  .argument("<componentName>", "component name")
+  .argument("<configFilePath>", "path to the configuration file")
+  .action((componentName, configFilePath) => {
+    install(componentName, configFilePath);
+  });  
+
+  program
+  .command("update")
+  .description("update the specified component on kubernetes")
+  .argument("<componentName>", "component name")
+  .argument("<configFilePath>", "path to the configuration file")
+  .action((componentName, configFilePath) => {
+    update(componentName, configFilePath);
+  });  
+
+  program
+  .command("remove")
+  .description("remove the specified component on kubernetes")
+  .argument("<componentName>", "component name")
+  .argument("<configFilePath>", "path to the configuration file")
+  .action((componentName, configFilePath) => {
+    remove(componentName, configFilePath);
+  });   
+  
+  
+  //create_config("argo-cd");
+  //install("argo-cd", "./argo-cd.yaml");
+  //update("argo-cd", "./argo-cd.yaml");
+  //remove("argo-cd", "./argo-cd.yaml");
+
+  program.parse();
 })();
