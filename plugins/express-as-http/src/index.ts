9616ba7f2824cf34c66fa8280d7329ebc909ee6f
sakshiarora <sakshiarora386@gmail.com>
2024-09-03 13:32:45 +0530

    added github oauth2 code

--- plugins/express-as-http/src/index.ts
+++ plugins/express-as-http/src/index.ts
@@ -2,11 +2,14 @@ import { PlainObject, GSActor, GSCloudEvent, GSStatus, GSEventSource, logger } f
 import express from "express";
 import bodyParser from 'body-parser';
 import promClient from '@godspeedsystems/metrics';
+import cors from 'cors';
 //@ts-ignore
 import promMid from '@godspeedsystems/express-prometheus-middleware';
 import passport from "passport";
-import fileUpload from "express-fileupload"
+const session = require('express-session');
+import fileUpload from "express-fileupload";
 import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
+import { Strategy as GithubStrategy } from 'passport-github2';
 
 export default class EventSource extends GSEventSource {
   async initClient(): Promise<PlainObject> {
@@ -16,8 +19,53 @@ export default class EventSource extends GSEventSource {
       request_body_limit = 50 * 1024 * 1024,
       file_size_limit = 50 * 1024 * 1024,
     } = this.config;
+    
+    // dotenv.config();
     const jwtConfig = this.config.authn?.jwt || this.config.jwt;
-
+    const githubConfig = this.config.authn.oauth2?.github || this.config.oauth2.github;
+    app.use(cors());
+    app.use(session({
+      secret: 'mysecret',
+      resave: false,
+      saveUninitialized: false
+    }));
+    
+    if (githubConfig) {
+      if (!githubConfig.client_id || !githubConfig.client_secret || !githubConfig.callback_url) {
+        logger.fatal('Github Setting error in http event source. Check all three Github settings are set properly for Express HTTP event source: client_id, client_secret or callback_url. Exiting');
+        process.exit(1);
+      }
+      app.use(passport.initialize());
+      app.use(passport.session());
+      passport.use(
+        new GithubStrategy(
+          {
+            clientID: githubConfig.client_id,
+            clientSecret: githubConfig.client_secret,
+            callbackURL: githubConfig.callback_url,
+            scope: ['user:email']
+          },
+          async function (accessToken: any, refreshToken: any, profile: any, done: any) {
+            return done(null, profile);
+          }
+        ),
+      );
+    }
+    // Authentication routes for github login
+    app.get('/login/github', passport.authenticate('github', { session: true, scope: ['user:email'] }), (req, res) => {
+    });
+    app.get('/auth/github/callback', passport.authenticate('github'), 
+    async (req, res) => {
+        const redirectUrl = '/verify/user' ;
+        res.redirect(redirectUrl);    
+      }
+    );  
+    passport.serializeUser(function (user, done) {
+      done(null, user);
+    });
+    passport.deserializeUser(function (obj: any, done) {
+      done(null, obj);
+    });
     app.use(bodyParser.urlencoded({ extended: true, limit: request_body_limit }));
     app.use(bodyParser.json({ limit: file_size_limit }));
     app.use(
@@ -25,7 +73,7 @@ export default class EventSource extends GSEventSource {
         useTempFiles: true,
         //@ts-ignore
         limits: { fileSize: file_size_limit },
-        abortOnLimit:true,
+        // abortOnLimit:true,
       })
     );
   
@@ -35,6 +83,7 @@ export default class EventSource extends GSEventSource {
         process.exit(1);
       }
       app.use(passport.initialize());
+      app.use(passport.session());
       passport.use(
         new JwtStrategy(
           {
@@ -66,20 +115,20 @@ export default class EventSource extends GSEventSource {
         })
       );
     }
-
     return app;
   }
 
+
   private authnHOF(authn: boolean) {
     return (req: express.Request, res: express.Response, next: express.NextFunction) => {
-      if (authn !== false && (this.config.authn?.jwt || this.config.authn)) {
-        return passport.authenticate('jwt', { session: false })(req, res, next)
-      } else {
-        next();
+      if (authn !== false && this.config.authn) {
+        // if authn is true and authn config exists
+        return passport.authenticate('jwt', { session: false })(req, res, next);
+      } else{
+             next();           // No authentication required       
       }
     };
-  };
-
+  }
   subscribeToEvent(eventRoute: string, eventConfig: PlainObject, processEvent: (event: GSCloudEvent, eventConfig: PlainObject) => Promise<GSStatus>, event?: PlainObject): Promise<void> {
     const routeSplit = eventRoute.split('.');
     const httpMethod: string = routeSplit[1];
@@ -94,12 +143,12 @@ export default class EventSource extends GSEventSource {
       // baseUrl = trimSlashes(baseUrl);
       // baseUrl = baseUrl.replace(/^\//,''); //remove starting /
       // baseUrl = baseUrl.replace(/^\//,''); //remove starting /
-      fullUrl = "/" + baseUrl  + "/" + endpoint;
+      fullUrl = "/" + baseUrl + "/" + endpoint;
       fullUrl = fullUrl.replace(/\/\//g, '/');
     } else {
       fullUrl = endpoint;
     }
-    
+
     const app: express.Express = this.client as express.Express;
     //@ts-ignore
     app[httpMethod](fullUrl, this.authnHOF(event.authn), async (req: express.Request, res: express.Response) => {
@@ -119,8 +168,8 @@ function trimSlashes(endpoint: string) {
   if (endpoint[0] === '/') {
     endpoint = endpoint.substring(1);
   }
-  if (endpoint[endpoint.length -1] === '/') {
-    endpoint = endpoint.substring(0, endpoint.length -1);
+  if (endpoint[endpoint.length - 1] === '/') {
+    endpoint = endpoint.substring(0, endpoint.length - 1);
   }
   return endpoint;
 }
@@ -151,16 +200,16 @@ function createGSEvent(req: express.Request, endpoint: string) {
   return event;
 }
 
-function pick (o:PlainObject, keys:string[]): PlainObject {
-  let newObj:PlainObject = {};
+function pick(o: PlainObject, keys: string[]): PlainObject {
+  let newObj: PlainObject = {};
   for (let key of keys) {
     newObj[key] = o[key];
   }
   return newObj; //return new copy
 }
 
-function omit (o:PlainObject, keys:string[]): PlainObject {
-  o = {...o}; //shallow clone
+function omit(o: PlainObject, keys: string[]): PlainObject {
+  o = { ...o }; //shallow clone
   for (let key of keys) {
     delete o[key];
   }
@@ -172,7 +221,7 @@ const Type = 'express'; // this is the loader file of the plugin, So the final l
 const CONFIG_FILE_NAME = 'http'; // in case of event source, this also works as event identifier, and in case of datasource works as datasource name
 const DEFAULT_CONFIG = { port: 3000, docs: { endpoint: '/api-docs' } };
 
-export  {
+export {
   EventSource,
   SourceType,
   Type,
