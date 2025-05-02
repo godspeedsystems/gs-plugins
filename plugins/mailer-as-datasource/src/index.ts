1a069c88e39bc866aad018ca099a11bfc7e00916
Sakshi Godspeed <sakshiarora386@gmail.com>
2025-05-02 15:08:13 +0530

    remove fnNameInWorkflow

--- plugins/mailer-as-datasource/src/index.ts
+++ plugins/mailer-as-datasource/src/index.ts
@@ -2,59 +2,61 @@ import { GSContext,  GSDataSource, PlainObject,} from "@godspeedsystems/core";
 import nodemailer from 'nodemailer';
 
 export default class DataSource extends GSDataSource {
-protected async initClient(): Promise<object> {
-  return nodemailer;
-}
-
-async execute(ctx: GSContext, args: PlainObject): Promise<any> {
-    
-  try {
-    const {
-      from,
-      to,
-      subject,
-      text,
-      meta: { fnNameInWorkflow },
-    } = args;
-    let method = fnNameInWorkflow.split(".")[2];
-    if (this.client) {
-      if(method == "send"){
-        const transporter = nodemailer.createTransport({
-          service: 'gmail',
-          auth: {
-            user: this.config.user,
-            pass: this.config.pass // Use the App Password you generated
-          }
-        });
-        const mailOptions = {
-          from: from,
-          to: to,
-          subject: subject,
-          text: text
-        };
-        
-        // Send the email
-        transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-            console.log(error);
-          } else {
-            console.log(info.response);
-          }
-        });
-      }else{
-        return 'Invalid method'
-      }
-      
-    }
-  } catch (error) {
-    throw error;
+  protected async initClient(): Promise<object> {
+    return nodemailer;
   }
-}
+
+  async execute(ctx: GSContext, args: PlainObject): Promise<any> {  
+    try {
+      const {
+        from,
+        to,
+        subject,
+        text,
+      } = args;
+      const method = args.method ?? "send";
+      
+    // let method = fnNameInWorkflow?.split(".")[2]; // for yaml workflows extract method from fn field
+
+      if (this.client) {
+        if(method == "send"){
+          const transporter = nodemailer.createTransport({
+
+            service: this.config?.service || 'gmail',
+            host: this.config?.host,
+            port: this.config?.port,
+            secure: this.config?.secure, // true for 465, false for other ports
+            auth:{
+              user: this.config.user,
+              pass: this.config.pass
+            }     
+          });
+          const mailOptions = {
+            from: from,
+            to: to,
+            subject: subject,
+            text: text
+          };
+          
+          // Send the email
+          const info = await transporter.sendMail(mailOptions);
+          
+          return info; // Return the result
+        } else {
+          return 'Invalid method';
+        }
+
+      }
+    } catch (error) {
+      console.error('Error in mailer datasource:', error); // Log error in datasource
+      throw error;
+    }
+  }
 }
 const SourceType = 'DS';
 const Type = "mailer"; // this is the loader file of the plugin, So the final loader file will be `types/${Type.js}`
 const CONFIG_FILE_NAME = "mailer"; // in case of event source, this also works as event identifier, and in case of datasource works as datasource name
-const DEFAULT_CONFIG = {};
+const DEFAULT_CONFIG = {type: 'mailer', service: 'gmail', user: '', pass: ''};
 
 export {
   DataSource,
